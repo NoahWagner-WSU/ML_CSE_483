a346767e41e11438c695ab5db37827940aac8e08
Noah Wagner <wagnernoah@icloud.com>
2023-09-29 20:31:50 -0700

    got to the 0.8 threshold

--- Assignment_2/assignment2.py
+++ Assignment_2/assignment2.py
@@ -9,20 +9,36 @@ from sklearn.compose import TransformedTargetRegressor
 from sklearn.model_selection import GridSearchCV
 
 class SelectColumns(BaseEstimator, TransformerMixin):
-	def __init__( self, columns ):
+	def __init__(self, columns):
 		self.columns = columns
 	# don't need to do anything
 	def fit(self, xs, ys, **params):
 		return self
 	# actually perform the selection
 	def transform(self, xs):
-		return xs[self.columns]
+		return xs[self.columns].fillna(0)
 
-grid = { 
+# class ExpandColumns(BaseEstimator, TransformerMixin):
+# 	def __init__(self, columns):
+# 		self.columns = columns
+# 	# don't need to do anything
+# 	def fit(self, xs, ys, **params):
+# 		return self
+# 	# actually perform the selection
+# 	def transform(self, xs):
+# 		return xs.join(pd.get_dummies(xs[self.columns], dtype=float))
+
+grid = {
+	# "column_expand__columns" : [
+	# 	["Kitchen Qual", "Neighborhood"]
+	# ],
 	"column_select__columns": [
-		["Gr Liv Area"],
-		["Gr Liv Area", "Overall Qual"],
-		["Gr Liv Area", "Overall Qual", "Year Built"],
+		["Overall Qual"],
+		["Overall Qual", "TotRms AbvGrd"],
+		["Overall Qual", "TotRms AbvGrd", "Full Bath", "Garage Cars"],
+		["Full Bath", "Garage Cars", "TotRms AbvGrd", "Fireplaces", "Year Built", "Overall Qual"],
+		["Full Bath", "Garage Cars", "TotRms AbvGrd", "Fireplaces", "Year Built", "Overall Qual", "Kitchen Qual_Ex", "Kitchen Qual_Gd"],
+		["Full Bath", "Garage Cars", "TotRms AbvGrd", "Fireplaces", "Year Built", "Overall Qual", "Kitchen Qual_Ex", "Kitchen Qual_Gd", "Neighborhood_NridgHt"]
 	],
 	"linear_regression": [
 		LinearRegression(n_jobs = -1), # no transformation
@@ -38,10 +54,19 @@ grid = {
 			LinearRegression(n_jobs = -1),
 			func = np.log,
 			inverse_func = np.exp),
+		TransformedTargetRegressor(
+			LinearRegression(n_jobs = -1),
+			func = lambda y: np.power(y, 1/1.5),
+			inverse_func = lambda y: np.power(y, 1.5)),
+		TransformedTargetRegressor(
+			LinearRegression(n_jobs = -1),
+			func = lambda y: np.power(y, 1/2.5),
+			inverse_func = lambda y: np.power(y, 2.5))
 	]
 }
 
 steps = [
+	# ("column_expand", ExpandColumns([])),
 	("column_select", SelectColumns([])),
 	("linear_regression", None),
 ]
@@ -52,7 +77,8 @@ search = GridSearchCV(pipe, grid, scoring = 'r2', n_jobs = -1)
 
 data = pd.read_csv("AmesHousing.csv")
 
-xs = data.drop( columns = ["SalePrice"])
+xs = data.drop(columns = ["SalePrice"])
+xs = xs.join(pd.get_dummies(xs[["Kitchen Qual", "Neighborhood"]], dtype=float))
 ys = data["SalePrice"]
 train_x, test_x, train_y, test_y = train_test_split(xs, ys, train_size = 0.7)
 
