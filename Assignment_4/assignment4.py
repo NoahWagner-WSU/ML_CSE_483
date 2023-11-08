d0a1b72d476ab0826144d214e4cbf93998fe7618
Noah Wagner <wagnernoah@icloud.com>
2023-11-08 11:52:40 -0800

    added outline for rest of project

--- Assignment_4/assignment4.py
+++ Assignment_4/assignment4.py
@@ -6,6 +6,17 @@ from sklearn.base import TransformerMixin, BaseEstimator
 from sklearn.model_selection import GridSearchCV
 from sklearn.metrics import silhouette_score
 from sklearn.cluster import KMeans
+from sklearn.preprocessing import MinMaxScaler
+
+class SelectColumns(BaseEstimator, TransformerMixin):
+	def __init__(self, columns):
+		self.columns = columns
+	# don't need to do anything
+	def fit(self, xs, ys, **params):
+		return self
+	# actually perform the selection
+	def transform(self, xs):
+		return xs[self.columns]
 
 data = pd.read_csv("Pokemon.csv")
 
@@ -14,14 +25,32 @@ cluster_range = range(2, 15)
 types = data["Type 1"].unique()
 type_instances = {}
 
+type_clusters = {}
+
 for typ in types:
 	type_instances[typ] = data.loc[data["Type 1"] == typ]
 
-print(type_instances)
+steps = {
+	"column_select": SelectColumns(["HP", "Attack", "Defense", "Sp. Atk", "Sp. Def", "Speed"]),
+	"minmax": MinMaxScaler(),
+	"cluster": KMeans(),
+}
 
-# def main():
+pipeline = Pipeline(steps)
 
+def main():
+	for pok_type in types:
+		print_cluster_scores(pok_type, cluster(pok_type))
+
+	# print the clusters
+
+# converts the pipeline output for a pokimon type into a dataframe for each cluster, then prints each dataframe
+# print_cluster(type)
+
+# prints the clusters silhouette_scores in the specific output format
 # def print_cluster_scores(type, silhouette_scores):
 
-# def check_n_clusters(type):
-	# return array of silhouette_score
+# try fitting the pipline for cluster__n_cluster in cluster_range
+# store the best silhouette clusters in type_clusters[type], so type_clusters[type] = pipeline.predict(x)
+# def cluster(type):
+	# return array of silhouette_scores for each n_cluster in cluster_range, whose index i cooresponds to the ith number in cluster range
